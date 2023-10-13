1aa77578bda925b29758010b4a3f3fcf98ae53df
Simon Rolph <simon.p.rolph@gmail.com>
2023-10-13 14:23:16 +0100

    gitignore model outputs and update sbatch file

--- biodiversity_model/submit_multiple_demo.sh
+++ biodiversity_model/submit_multiple_demo.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH --job-name=ecosystem_services_biodiversity
 #SBATCH --account=project_465000357
-#SBATCH --time=00:10:00
-#SBATCH --array=1-10
+#SBATCH --time=00:20:00
+#SBATCH --array=1-100
 #SBATCH --output=hpc_logs/%A_%a.out
 #SBATCH --error=hpc_logs/%A_%a.err
 #SBATCH --nodes=1
@@ -13,7 +13,7 @@
 #SBATCH --mail-type=all
 
 # Read the numeric ID from the text file based on the array task ID
-ID=$(sed -n "${SLURM_ARRAY_TASK_ID}p" taxonIDs.txt)
+ID=$(sed -n "${SLURM_ARRAY_TASK_ID}p" species_info/taxonIDs.txt)
 echo "Processing ID: $ID"
 
-singularity exec --bind "$PWD" /projappl/project_465000357/simonrolph/ces-biodiversity_0.1.0.sif Rscript run_biodiversity_model.R $ID outputs/maps outputs/reports 5
+singularity exec --bind "$PWD" /projappl/project_465000357/simonrolph/ces-biodiversity_0.2.0.sif Rscript run_biodiversity_model.R $ID outputs/maps outputs/reports 5
