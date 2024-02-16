fc27403646e6242e612e34c70e6b51fe07a704f5
Simon Rolph <simon.p.rolph@gmail.com>
2024-02-16 10:39:38 +0000

    Workflow diagrams, commented setwd() lines from rmd workflow files, utility scriptfor getting species lsit from gbif and image urls from inaturalist.

--- biodiversity_model/util/get_sp_list.R
+++ biodiversity_model/util/get_sp_list.R
@@ -1,5 +1,6 @@
 ## script to get a list of species with useable data from GBIF
 library(rgbif)
+library(dplyr)
 
 
 # all of scotland
@@ -29,8 +30,6 @@ species2$sci_name <- ""
 
 species2 <- species2[species2$count>100,]
 
-View(species2)
-
 #get common name
 library(httr)
 library(jsonlite)
@@ -44,11 +43,22 @@ get_common_name <- function(species_key){
     stop("Error: Unable to retrieve data.")
   }
   list(common_name = data$vernacularName,
-       sci_name = data$species)
-  
+       sci_name = data$species,
+       kingdom = data$kingdom,
+       phylum = data$phylum,
+       class = data$class,
+       order = data$order,
+       family = data$family,
+       genus = data$genus)
 }
 
 get_common_name(3314213)
+
+value_or_na <- function(x){if(is.null(x)){
+  ""
+}else{
+  x
+}}
 
 for(i in 1:nrow(species2)){
   gcn <- get_common_name(species2$speciesKey[i])
@@ -58,11 +68,79 @@ for(i in 1:nrow(species2)){
   } else {
     species2$common_name[i] <- "NA"
   }
-  species2$sci_name[i] <- gcn$sci_name
+  species2$sci_name[i] <- value_or_na(gcn$sci_name)
+  
+  species2$kingdom[i] <- value_or_na(gcn$kingdom )
+  species2$class[i] <- value_or_na(gcn$class)
+  species2$phylum[i] <- value_or_na(gcn$phylum )
+  species2$order[i] <- value_or_na(gcn$order)
+  species2$family[i] <- value_or_na(gcn$family)
+  species2$genus[i] <- value_or_na(gcn$genus)
 }
 
 View(species2)
 
-write.table(species2,"biodiversity_model/outputs/cairngorms_sp_list.csv", sep = ",",row.names = F)
+species3 <- species2
 
-writeLines(as.character(species2$speciesKey),"taxonIDs.txt")
+# append generic groups
+#create columns
+species3[,c("all","big5","mammals","birds","plants","herptiles","insects")] <- F
+species3$all <- T
+species3$big5[species3$sci_name %in% c("Sciurus vulgaris","Cervus elaphus")] <- T
+species3$mammals[species3$class =="Mammalia"] <- T
+species3$birds[species3$class =="Aves"] <- T
+species3$plants[species3$kingdom =="Plantae"] <- T
+species3$insects[species3$class =="Insecta"] <- T
+
+
+
+
+# get species images
+get_image <- function(sci_name){
+  api_url <- paste0("https://api.inaturalist.org/v1/taxa?rank_level=10&per_page=1&order=desc&order_by=observations_count&q=", gsub(" ","%20",sci_name))
+  response <- GET(api_url)
+  out <- if (http_type(response) == "application/json") {
+    data <- fromJSON(rawToChar(response$content))
+  } else {
+    stop("Error: Unable to retrieve data.")
+  }
+  
+  image_url <- data$results$default_photo$medium_url
+  
+  if(!is.null(image_url)){
+    return(image_url)
+  } else {
+    return("")
+  }
+
+}
+
+species3$image_url<-""
+for(i in 326:nrow(species3)){
+  Sys.sleep(1)
+  species3$image_url[i] <- get_image(species3$sci_name[i])
+  cat(i)
+}
+
+
+View(species3)
+
+write.table(species3,"biodiversity_model/outputs/cairngorms_sp_list.csv", sep = ",",row.names = F)
+
+writeLines(as.character(species3$speciesKey),"biodiversity_model/inputs/taxonIDs.txt")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
